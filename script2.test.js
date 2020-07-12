8dc5a3dafcc2f7331d4cf498c8209de771655d01
Nimbo1999 <matlopes1999@gmail.com>
2020-07-12 12:51:27 -0300

    + Add: mock data for asyncronous tests

--- script2.test.js
+++ script2.test.js
@@ -26,3 +26,27 @@ describe('Swapi API Test Call', () => {
   });
 
 });
+
+describe('Test with mock data', () => {
+
+  it('getPeople return count and result', () => {
+
+    const mochFetch = jest.fn()
+      .mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        })
+      }));
+
+    expect.assertions(4);
+    return swapi(mochFetch).then(data => {
+      expect(mochFetch.mock.calls.length).toBe(1);
+      expect(mochFetch).toBeCalledWith('https://swapi.py4e.com/api/people');
+      expect(data.count).toBe(87);
+      expect(data.results.length).toBeGreaterThan(9);
+    });
+    
+  });
+
+});
